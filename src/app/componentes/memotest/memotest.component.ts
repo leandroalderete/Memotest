4aba87feaa54b7fdc5bf37b5dae94a0fe2c35039
Ezequiel <ezequieloggioni@gmail.com>
2024-04-16 10:39:14 -0300

    se agrego se pasaron los datos por input

--- src/app/componentes/memotest/memotest.component.ts
+++ src/app/componentes/memotest/memotest.component.ts
@@ -1,17 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Pieza } from '../../Clases/pieza';
+import { PiezaComponent } from '../pieza/pieza.component';
 
 @Component({
   selector: 'app-memotest',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PiezaComponent],
   templateUrl: './memotest.component.html',
   styleUrl: './memotest.component.css'
 })
 export class MemotestComponent {
   public piezas: Array<Pieza> = new Array<Pieza>();
-  public dorso: string = "https://imgs.search.brave.com/hS-3fAVxnh2IuEhl0DJ3CTst5XyuIHsuZbX-XuezqA0/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/Zm90by1ncmF0aXMv/dGV4dHVyYS1wYXJl/ZC1sYWRyaWxsb18y/My0yMTQ3OTYwMjQ2/LmpwZz9zaXplPTYy/NiZleHQ9anBn";
 
   constructor() {
     let numeros: Number[] = new Array<Number>();
